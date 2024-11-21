6a05019f328cbf0cb3535668edf316896fd66dc3
tavinxt <140723672+tavinxt@users.noreply.github.com>
2024-11-21 08:18:28 -0300

    Add files via upload

---
+++ Final-main/PORTIFÓLIO/3 TRIMESTRE/perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'HANDEBOL',
+    'Quantos passos um jogador pode dar com a bola?',
+    'No handebol, um jogador pode dar até três passos com a bola sem driblar.'
+)
+
+criaCartao(
+    'FUTSAL',
+    'Quem é considerado um dos maiores jogadores de futsal de todos os tempos?',
+    'Falcão é amplamente reconhecido como um dos maiores jogadores de futsal da história.'
+)
+
+criaCartao(
+    'VÔLEI',
+    'Qual é a posição do jogador que recebe o saque e faz passes?',
+    'O jogador que recebe o saque e faz passes é geralmente chamado de levantador.'
+)
+
+criaCartao(
+    'BASQUETE',
+    'Quantos períodos tem uma partida de basquete na NBA?',
+    'Uma partida da NBA é composta por 4 períodos de 12 minutos cada.'
+)
+
